ddc0012a9f3484567129a1b4f31542d3e325e5f2
Biswajit Paria <biswajitsc@gmail.com>
2021-01-26 13:28:56 -0500

    Training on unscaled data now

--- run_scripts/fixed_no_reg.sh
+++ run_scripts/fixed_no_reg.sh
@@ -6,7 +6,8 @@ do
     echo "RUN ${i}"
     echo
     CUDA_VISIBLE_DEVICES="3" python train.py \
-    --expt=node_emb_64/run_${i} --model=fixed_df --random_seed=${i} \
+    --expt=node_emb_24_rs_out/run_${i} --model=fixed_df --random_seed=${i} \
+    --node_emb_dim=24 --fixed_lstm_hidden=24 \
     --batch_size=500 --train_epochs=25 --patience=5 --learning_rate=0.01
     # break  # Comment when running multiple expts
 done
