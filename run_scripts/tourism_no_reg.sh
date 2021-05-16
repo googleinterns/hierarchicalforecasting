c3ac6499d29f4bca218b045fabe3a31cbbc4ae11
Biswajit Paria <biswajitsc@gmail.com>
2021-05-15 21:41:30 -0400

    Updating lstm size for deep factors

--- run_scripts/tourism_no_reg.sh
+++ run_scripts/tourism_no_reg.sh
@@ -6,8 +6,8 @@ do
     echo "RUN ${i}"
     echo
     CUDA_VISIBLE_DEVICES="2" python train.py \
-    --expt=hid_10_exp_4_norm/run_${i} --dataset='tourism' --random_seed=${i} \
-    --fixed_lstm_hidden=10 --node_emb_dim=4 \
+    --expt=hid_20_exp_4_norm/run_${i} --dataset='tourism' --random_seed=${i} \
+    --fixed_lstm_hidden=20 --node_emb_dim=4 \
     --batch_size=512 --train_epochs=40 --patience=10 --learning_rate=0.1 --num_changes=6 \
     --hist_len=24 --train_pred=4 --test_pred=4 --val_windows=3 --test_windows=3
 done
